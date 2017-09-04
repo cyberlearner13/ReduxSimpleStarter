be253d714329fd4dbce01003a051d76a8bdc30eb
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2017-09-04 21:21:33 +0530

    reducer not updating state hence react component re-render is not taking place

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '8cb83d364fbca35eb87a12a92e5d0bd6';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?APPID=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
---
+++ src/containers/searchBar.js
@@ -0,0 +1,46 @@
+import React,{ Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      term:''
+    };
+  }
+
+  onInputChange(e){
+    this.setState({term:e.target.value});
+  }
+
+  onFormSubmit(e){
+    e.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({term:''});
+  }
+  render(){
+    return(
+      <form onSubmit={this.onFormSubmit.bind(this)} className="input-group">
+        <input
+          placeholder = 'Get a 5 day forecast in your favourite cities'
+          className = "form-control"
+          value = {this.state.term}
+          onChange={this.onInputChange.bind(this)}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">
+            Submit
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(SearchBar);
+//null: this container carest not 'bout thy state, thus we passeth the null value
